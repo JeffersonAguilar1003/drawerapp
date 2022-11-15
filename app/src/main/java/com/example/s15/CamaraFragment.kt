82509ca34bd22fd01f32754dcaa3a3194286a1bb
JeffersonAguilar1003 <113949238+JeffersonAguilar1003@users.noreply.github.com>
2022-11-14 21:59:15 -0600

    drawerapp

---
+++ app/src/main/java/com/example/s15/CamaraFragment.kt
@@ -0,0 +1,12 @@
+package com.example.s15
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CamaraFragment : Fragment(R.layout.fragmen_camara) {
+    
+}
---
+++ app/src/main/java/com/example/s15/GaleriaFragment.kt
@@ -0,0 +1,12 @@
+package com.example.s15
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class GaleriaFragment : Fragment(R.layout.fragmen_galeria) {
+
+}
